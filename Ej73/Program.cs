07cfc2c81678844a6819492cdada6935044ff0b1
Euge <Euge@DESKTOP-S716L4F>
2022-11-17 21:05:26 -0300

    Agregar archivos de proyecto.

---
+++ Ej73/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej73
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Cad;
+
+            Queue colastring = new Queue();
+
+            Console.WriteLine("ingrese la cadena a agregar a la cola:");
+
+            Cad = Console.ReadLine();
+
+
+            while (Cad != string.Empty)
+            {
+                colastring.Enqueue(Cad);
+                Console.WriteLine("Ingrese próxima cadena:");
+                Cad = Console.ReadLine();
+            }
+
+            while (colastring.Count > 0)
+            {
+                
+                string cadena = (string) colastring.Dequeue();
+                Console.WriteLine(cadena);
+            }
+
+           
+
+            Console.ReadKey();
+
+        }
+    }
+}
